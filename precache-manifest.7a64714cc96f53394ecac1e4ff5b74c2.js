da16c06e1e0c1bfff4ae15c484f34cc6ad119604
shrey <crazymeshrey@gmail.com>
2020-08-02 18:13:11 +0530

    Updates

---
+++ precache-manifest.7a64714cc96f53394ecac1e4ff5b74c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bb6e124ae9682a8e6a325b3f5c82cc",
+    "url": "/Covid-19-tracker/index.html"
+  },
+  {
+    "revision": "e9cdfc56759d544e1055",
+    "url": "/Covid-19-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4262fe1191d2ddd8d24f",
+    "url": "/Covid-19-tracker/static/css/main.bb498e4b.chunk.css"
+  },
+  {
+    "revision": "e9cdfc56759d544e1055",
+    "url": "/Covid-19-tracker/static/js/2.fd8f95b6.chunk.js"
+  },
+  {
+    "revision": "06abe5989ed4dece94a2845019635433",
+    "url": "/Covid-19-tracker/static/js/2.fd8f95b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4262fe1191d2ddd8d24f",
+    "url": "/Covid-19-tracker/static/js/main.515e78b2.chunk.js"
+  },
+  {
+    "revision": "6a7d2603062c3a4dca49",
+    "url": "/Covid-19-tracker/static/js/runtime-main.de7780b2.js"
+  }
+]);
