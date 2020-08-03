2d6c8becdc83bd49cee561c201b5b2d3bf0f2ac4
beingyourself <crazymeshrey@gmail.com>
2020-08-03 16:11:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import './App.css';
 import RouterIndex from './Routes/index'
-import Chart from 'chart.js'
 class App extends React.Component {
 
-componentDidMount(){
-  let ctx=this.weight;
-  console.log(ctx);
-}
+
   render() {
     return (
       <React.Fragment>
         <RouterIndex />
-        <canvas className="chart" ref={ref=>this.weight=ref}>
-
-        </canvas>
       </React.Fragment>
 
     );
